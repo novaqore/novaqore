6fb1d65f7bd56b0a4d407a71a6008b99ccd0548f
novaqoredev <dev@novaqore.tech>
2025-04-08 10:01:17 -0700

    Create AuthContext.js
    Auth context for firebase and react apps.

---
+++ react/contexts/AuthContext.js
@@ -0,0 +1,29 @@
+"use client";
+
+import { auth } from "@/lib/firebase/init";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
