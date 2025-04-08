ca4111a0836e33cd0d0b0615703a03d46899d784
novaqoredev <dev@novaqore.tech>
2025-04-08 09:57:24 -0700

    Create admin.js
    Firebase admin init and export.

---
+++ firebase/admin.js
@@ -0,0 +1,17 @@
+import * as admin from 'firebase-admin';
+
+// Default Firebase Admin App
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+  });
+}
+
+
+
+export { admin };
