28748adb3cb4dbfcdf0308b39aec5532abb0ba2f
novaqoredev <dev@novaqore.tech>
2025-04-21 23:18:52 -0700

    Create gpu.sh

---
+++ aws/ec2/gpu.sh
@@ -0,0 +1,3 @@
+wget https://repo.radeon.com/amdgpu-install/6.3.4/ubuntu/noble/amdgpu-install_6.3.60304-1_all.deb #Ubuntu 24
+sudo chown _apt:root amdgpu-install_6.3.60304-1_all.deb
+sudo apt install ./amdgpu-install_6.3.60304-1_all.deb
