7fb00c2a0b81a98a8e1526c1314e734b4abcc70b
novaqoredev <dev@novaqore.tech>
2025-04-23 11:35:40 -0700

    Update gpu.sh

--- aws/ec2/gpu.sh
+++ aws/ec2/gpu.sh
@@ -1,18 +1,14 @@
-lspci -nn | grep -E 'VGA|Display' #Check for GPUs
-lspci -nn | grep -i "AMD\|Radeon" #Check AMD Radeon
-#Advanced Micro Devices, Inc. [AMD/ATI] Navi 12 [Radeon Pro V520/V540] [1002:7362] (rev c3)
-
-lsmod | grep amdgpu #Check Kernel Driver
-
-sudo apt-get update --fix-missing && sudo apt-get upgrade -y
-sudo apt install build-essential -y
-sudo apt install linux-firmware linux-modules-extra-aws -y
-sudo reboot
-
-wget https://repo.radeon.com/amdgpu-install/6.3.4/ubuntu/noble/amdgpu-install_6.3.60304-1_all.deb #Ubuntu 24
-sudo chown _apt:root amdgpu-install_6.3.60304-1_all.deb
-sudo apt install ./amdgpu-install_6.3.60304-1_all.deb
-
-amdgpu-install --usecase=workstation --vulkan=pro -y
-sudo reboot
-sudo dmesg | grep amdgpu
+# https://docs.nvidia.com/cuda/cuda-installation-guide-linux/
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=24.04&target_type=deb_local
+# g4dn Tesla T4
+lspci | grep -i nvidia
+uname -m && cat /etc/*release
+gcc --version
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-ubuntu2404.pin
+sudo mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda-repo-ubuntu2404-12-8-local_12.8.1-570.124.06-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2404-12-8-local_12.8.1-570.124.06-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2404-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-8
+sudo apt-get install -y nvidia-open
