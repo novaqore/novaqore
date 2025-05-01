fd318b40e538d84bcc21c244b2177398c0cd4e93
novaqoredev <dev@novaqore.tech>
2025-04-30 17:24:50 -0700

    Update admin.js

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,6 +1,8 @@
-import * as admin from 'firebase-admin';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Default Firebase Admin App
+import admin from 'firebase-admin';
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
@@ -8,10 +10,7 @@ if (!admin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
   });
 }
-
-
 
 export { admin };
